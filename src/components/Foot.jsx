0cb346f070c08337be5868319cc1a704baa84bc1
code-soham <soham36chowdhury@gmail.com>
2023-03-27 02:17:33 +0530

    social links

--- src/components/Foot.jsx
+++ src/components/Foot.jsx
@@ -9,12 +9,12 @@ export default function Footer() {
           id="links"
           className="flex flex-row justify-evenly min-w-fit w-2/6 align-middle"
         >
-          <a rel='noreferrer' target='_blank' href="https://www.facebook.com/CFI.JGEC">
+          <a rel='noreferrer' target='_blank' href="https://www.facebook.com/fiesta.events.2023?mibextid=ZbWKwL">
             <div className="bg-gray-700 bg-opacity-40 rounded-full h-8 w-8">
               <i className="bx p-2 text-blue-500 bxl-facebook"></i>
             </div>
           </a>
-          <a rel='noreferrer' target='_blank' href="https://www.instagram.com/cfi_jgec/">
+          <a rel='noreferrer' target='_blank' href="https://instagram.com/fiestaevents.co.in?igshid=ZDdkNTZiNTM=">
             <div className="bg-gray-700 bg-opacity-40 rounded-full h-8 w-8">
               <i className="bx p-2 text-fuchsia-700 bxl-instagram"></i>
             </div>
