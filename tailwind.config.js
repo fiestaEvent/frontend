bc40204a2f56ffad1e7266509c20530401d116b2
code-soham <soham36chowdhury@gmail.com>
2023-06-24 22:19:28 +0530

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,14 @@ module.exports = {
         "100%": { transform: "scale(1)" },
       },
     },
+    screens: {
+      xs: "475px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
