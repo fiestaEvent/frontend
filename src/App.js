f6c632034b039badfed09dac7d0642171596f50b
code-soham <soham36chowdhury@gmail.com>
2023-07-05 07:01:47 +0530

    helmet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Helmet from "react-helmet";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Nav";
 import Footer from "./components/Foot";
@@ -20,54 +20,59 @@ function App() {
     once: true,
   });
   return (
-    <div className="App bg-[url('https://i.imgur.com/qXVQ2nF.pn')] bg-fuchsia-50 bg-fixed bg-cover">
-      <Helmet>
-        <title>Fiesta | Events</title>
-        <meta
-          name="description"
-          content="We offer professional event management services for all types of occasions, including weddings, corporate events, and parties. Trust Fiesta to handle all aspects of your event planning, from venue selection to decor and entertainment. Make your event a success with us.From intimate gatherings to large-scale events, Fiesta creates experiences that leave a lasting impression. Contact us today to start planning your event."
-        />
+    <HelmetProvider>
+      <div className="App bg-[url('https://i.imgur.com/qXVQ2nF.pn')] bg-fuchsia-50 bg-fixed bg-cover">
+        <Helmet>
+          <title>Fiesta | Events</title>
+          <meta
+            name="description"
+            content="We offer professional event management services for all types of occasions, including weddings, corporate events, and parties. Trust Fiesta to handle all aspects of your event planning, from venue selection to decor and entertainment. Make your event a success with us.From intimate gatherings to large-scale events, Fiesta creates experiences that leave a lasting impression. Contact us today to start planning your event."
+          />
 
-        <meta itemprop="name" content="Fiesta - Event Management" />
-        <meta
-          itemprop="description"
-          content="We offer professional event management services for all types of occasions, including weddings, corporate events, and parties. Trust Fiesta to handle all aspects of your event planning, from venue selection to decor and entertainment. Make your event a success with us.From intimate gatherings to large-scale events, Fiesta creates experiences that leave a lasting impression. Contact us today to start planning your event."
-        />
-        <meta itemprop="image" content="https://i.imgur.com/7VjZu9w.jpg" />
+          <meta itemprop="name" content="Fiesta - Event Management" />
+          <meta
+            itemprop="description"
+            content="We offer professional event management services for all types of occasions, including weddings, corporate events, and parties. Trust Fiesta to handle all aspects of your event planning, from venue selection to decor and entertainment. Make your event a success with us.From intimate gatherings to large-scale events, Fiesta creates experiences that leave a lasting impression. Contact us today to start planning your event."
+          />
+          <meta itemprop="image" content="https://i.imgur.com/7VjZu9w.jpg" />
 
-        <meta property="og:url" content="https://www.fiestaevent.co.in" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Fiesta - Event Management" />
-        <meta
-          property="og:description"
-          content="We offer professional event management services for all types of occasions, including weddings, corporate events, and parties. Trust Fiesta to handle all aspects of your event planning, from venue selection to decor and entertainment. Make your event a success with us.From intimate gatherings to large-scale events, Fiesta creates experiences that leave a lasting impression. Contact us today to start planning your event."
-        />
-        <meta property="og:image" content="https://i.imgur.com/7VjZu9w.jpg" />
+          <meta property="og:url" content="https://www.fiestaevent.co.in" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Fiesta - Event Management" />
+          <meta
+            property="og:description"
+            content="We offer professional event management services for all types of occasions, including weddings, corporate events, and parties. Trust Fiesta to handle all aspects of your event planning, from venue selection to decor and entertainment. Make your event a success with us.From intimate gatherings to large-scale events, Fiesta creates experiences that leave a lasting impression. Contact us today to start planning your event."
+          />
+          <meta property="og:image" content="https://i.imgur.com/7VjZu9w.jpg" />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Fiesta - Event Management" />
-        <meta
-          name="twitter:description"
-          content="We offer professional event management services for all types of occasions, including weddings, corporate events, and parties. Trust Fiesta to handle all aspects of your event planning, from venue selection to decor and entertainment. Make your event a success with us.From intimate gatherings to large-scale events, Fiesta creates experiences that leave a lasting impression. Contact us today to start planning your event."
-        />
-        <meta name="twitter:image" content="https://i.imgur.com/7VjZu9w.jpg" />
-      </Helmet>
-      <HashRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/testimonials" element={<Testimonial />} />
-          <Route path="/blog/:b" element={<BlogView />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/" element={<Preloader />} />
-        </Routes>
-        <Footer />
-      </HashRouter>
-    </div>
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Fiesta - Event Management" />
+          <meta
+            name="twitter:description"
+            content="We offer professional event management services for all types of occasions, including weddings, corporate events, and parties. Trust Fiesta to handle all aspects of your event planning, from venue selection to decor and entertainment. Make your event a success with us.From intimate gatherings to large-scale events, Fiesta creates experiences that leave a lasting impression. Contact us today to start planning your event."
+          />
+          <meta
+            name="twitter:image"
+            content="https://i.imgur.com/7VjZu9w.jpg"
+          />
+        </Helmet>
+        <HashRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/testimonials" element={<Testimonial />} />
+            <Route path="/blog/:b" element={<BlogView />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Preloader />} />
+          </Routes>
+          <Footer />
+        </HashRouter>
+      </div>
+    </HelmetProvider>
   );
 }
 
--- src/views/BlogView.jsx
+++ src/views/BlogView.jsx
@@ -4,6 +4,7 @@ import { marked } from "marked";
 import { baseUrl } from "../utils/api";
 import EmailCollectModal from "../components/Modals/emailCollectModal";
 import ApiModal from "../components/Modals/apiModal";
+import { Helmet } from "react-helmet-async";
 export default function BlogView(props) {
   const [show, setShow] = useState(false);
   const [doneSubmit, setDoneSubmit] = useState(false);
@@ -32,7 +33,7 @@ export default function BlogView(props) {
       });
       const result = await res.json();
       setBlog(result.blog);
-      const html = await marked.parse(result.blog.body);
+      const html = await marked.parse(result.blog?.body);
       setState(html);
       setLoading(false);
       setShow(true);
@@ -41,6 +42,26 @@ export default function BlogView(props) {
   }, []);
   return (
     <div className="min-h-screen w-full flex flex-col justify-start py-20">
+      <Helmet>
+        <title>Fiesta | Events</title>
+        <meta name="description" content={blog?.preview} />
+
+        <meta itemprop="name" content="Fiesta - Event Management" />
+        <meta itemprop="description" content={blog?.preview} />
+        <meta itemprop="image" content={blog?.Image} />
+
+        <meta property="og:url" content={`https://www.fiestaevent.co.in/blog/${blog._id}`} />
+        <meta property="og:type" content="Blog Article" />
+        <meta property="og:title" content={blog?.title} />
+        <meta property="og:description" content={blog?.preview} />
+        <meta property="og:image" content={blog?.Image} />
+        <meta property="og:locale" content="en_GB" />
+d
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={NavigationPreloadManager?.title} />
+        <meta name="twitter:description" content={blog?.preview} />
+        <meta name="twitter:image" content={blog?.Image} />
+      </Helmet>
       {loading && <ApiModal />}
       <div className="w-full h-60 overflow-hidden">
         <img className="w-full object-cover" src={blog?.Image} alt="Header" />
